f6e112694cd9a491ef954ec1ceb494219c588e6d
ShutongLi <43733869+ShutongLi@users.noreply.github.com>
2019-11-24 02:20:50 -0800

    Initial commit

---
+++ charts.js
@@ -0,0 +1,119 @@
+
+var radar_chart_coord = {0: "Intelligence", 60: "Strength", 120:'Speed', 180: 'Durability', 240: 'Power', 300: 'Combat'}
+var radar_config = {
+
+    chart: {
+        polar: true,
+    },
+
+    title: {
+        text: 'Power scale: Spider-Man is more powerful than his classic nemesis',
+        style: {
+            fontSize: "25px",
+        }
+    },
+
+    subtitle: {
+        text: 'Comparison of Spider Man power metrics against the average of that of sinister six'
+    },
+
+    pane: {
+        startAngle: 0,
+        endAngle: 360
+    },
+
+    tooltip: {
+        shared:false,
+        borderWidth: 0,
+        backgroundColor: '#cccac9',
+        opacity:0.3,
+        pointFormat: '{point.y}',
+        headerFormat: '',
+        shadow: false,
+        style: {
+            fontSize: '10px'
+        },
+    },
+
+    xAxis: {
+        tickInterval: 60,
+        min: 0,
+        max: 360,
+        labels: {
+            formatter: function() {
+                console.log(radar_chart_coord[this.value])
+                return radar_chart_coord[this.value];
+                },
+            enabled: true,
+        }
+    },
+
+    yAxis: {
+        min: 0,
+        max: 100,
+    },
+
+    plotOptions: {
+        series: {
+            pointStart: 0,
+            pointInterval: 60
+        },
+        column: {
+            pointPadding: 0,
+            groupPadding: 0
+        }
+    },
+
+    series: [{
+        type: 'area',
+        name: 'Spider Man',
+        data: [88, 55, 60, 74, 58, 85],
+        color:'#9d0b0a',
+        opacity:0.5,
+    }, {
+        data:[59, 34.6, 40.4, 55.8, 46.2, 69.2],
+        name: 'Sinister Six',
+        color:'#436829',
+        opacity:0.5,
+        type: 'area',
+    }]
+};
+
+
+var word_cloud_config = {
+    series: [{
+        type: 'wordcloud',
+        data: [{name:'great',weight: 11},
+            {name:'comes', weight: 6} ,
+            {name:'power', weight: 6},
+            {name:'responsibility', weight: 5},
+            {name:'spider', weight: 5},
+            {name: 'get', weight: 4},
+            {name:'spiderman', weight: 4},
+            {name:'friendly', weight: 3},
+            {name:'man', weight: 3},
+            {name:'father', weight: 3},
+            {name:'back', weight: 2},
+            {name:'know', weight: 2},
+            {name:'picture', weight: 2},
+            {name:'parker', weight: 2},
+            {name:'ben', weight: 2},
+            {name:'name', weight: 2},
+            {name:'formula', weight: 2},
+            {name:'neighbourhood', weight: 2},
+            {name:'change', weight: 1},
+            {name:'life', weight: 1}],
+        name: 'Occurrences'
+    }],
+    title: {
+        text: 'Origin Story: Uncle Ben motifs',
+        style: {
+            fontSize: "25px",
+        }
+    },
+    subtitle: {
+        text: 'Word Cloud of famous quotes from Sam Raimi\'s Spider Man 1 ',
+    }
+}
+Highcharts.chart(document.getElementsByClassName('Radar_chart')[0], radar_config);
+Highcharts.chart(document.getElementsByClassName('word_cloud')[0], word_cloud_config);
---
+++ network.js
@@ -0,0 +1,170 @@
+var svg = d3.select("#chart1").append("svg").attr("width", 1900).attr("height", 850),
+    width = +svg.attr("width") - 100,
+    height = +svg.attr("height");
+var simulation = d3.forceSimulation()
+    .force("link", d3.forceLink().id(function(d) { return d.id; }))
+    //.force("charge", d3.forceManyBody().strength(-200))
+    .force('charge', d3.forceManyBody()
+        .strength(-4000)
+        .theta(0.1)
+        .distanceMax(400)
+    )
+    // 		.force('collide', d3.forceCollide()
+    //       .radius(d => 40)
+    //       .iterations(2)
+    //     )
+    .force("center", d3.forceCenter(width / 2, height / 2));
+
+
+const graph = {
+    "nodes": [
+        {"id": "peter parker", "group": 1},
+        {"id": "Aunt May", "group": 1},
+        {"id": "Happy Hogan", "group": 1},
+        //team tony
+        {"id": "tony stark", "group": 1},
+        {"id": "war machine", "group": 1},
+        {"id": "Natasha", "group": 1},
+        {"id": "Vision", "group": 1},
+        {"id": "Black Panther", "group": 1},
+        //school
+        {"id": "MJ", "group": 1},
+        {"id": "Ned Leeds", "group": 1},
+        {"id": "Flash Thompson", "group": 1},
+        {"id": "Liz", "group": 1},
+        //villain
+        {"id": "Vulture", "group": 3},
+        {"id": "Prowler", "group": 3},
+        //team cap
+        {"id": "Captain America", "group": 2},
+        {"id": "Falcon", "group": 2},
+        {"id": "Winter Soldier", "group": 2},
+        {"id": "Scarlet Witch", "group": 2},
+        {"id": "Hawkeye", "group": 2},
+        {"id": "Antman", "group": 2},
+    ],
+    "links": [
+        //hero friends
+        {"source": "peter parker", "target": "tony stark", "value": 1},
+        {"source": "peter parker", "target": "Aunt May", "value": 1},
+        {"source": "peter parker", "target": "Happy Hogan", "value": 1},
+        {"source": "Aunt May", "target": "Happy Hogan", "value": 1},
+        {"source": "tony stark", "target": "Happy Hogan", "value": 1},
+        //school friends
+        {"source": "peter parker", "target": "MJ", "value": 1},
+        {"source": "peter parker", "target": "Ned Leeds", "value": 1},
+        {"source": "peter parker", "target": "Flash Thompson", "value": 1},
+        {"source": "peter parker", "target": "Liz", "value": 1},
+        {"source": "MJ", "target": "Ned Leeds", "value": 1},
+        {"source": "MJ", "target": "Flash Thompson", "value": 1},
+        {"source": "MJ", "target": "Liz", "value": 1},
+        {"source": "Ned Leeds", "target": "Flash Thompson", "value": 1},
+        {"source": "Ned Leeds", "target": "Liz", "value": 1},
+        {"source": "Flash Thompson", "target": "Liz", "value": 1},
+        //villain rivalry
+        {"source": "peter parker", "target": "Vulture", "value": 1},
+        {"source": "peter parker", "target": "Prowler", "value": 1},
+        {"source": "Vulture", "target": "Prowler", "value": 1},
+        {"source": "Vulture", "target": "Liz", "value": 10},
+        //hero rivalry (team cap)
+        {"source": "peter parker", "target": "Captain America", "value": 1},
+        {"source": "tony stark", "target": "Captain America", "value": 1},
+        {"source": "Captain America", "target": "Falcon", "value": 1},
+        {"source": "Captain America", "target": "Scarlet Witch", "value": 1},
+        {"source": "Captain America", "target": "Hawkeye", "value": 1},
+        {"source": "Captain America", "target": "Antman", "value": 1},
+        {"source": "Captain America", "target": "Winter Soldier", "value": 1},
+        {"source": "Winter Soldier", "target": "Falcon", "value": 1},
+        //hero rivalry (team tony)
+        {"source": "tony stark", "target": "war machine", "value": 1},
+        {"source": "tony stark", "target": "Natasha", "value": 1},
+        {"source": "tony stark", "target": "Vision", "value": 1},
+        {"source": "tony stark", "target": "Black Panther", "value": 1},
+        //love hate
+        {"source": "Scarlet Witch", "target": "Vision", "value": 1},
+        {"source": "Natasha", "target": "Hawkeye", "value": 1},
+    ]
+}
+
+
+function run(graph) {
+
+    graph.links.forEach(function(d){
+//     d.source = d.source_id;
+//     d.target = d.target_id;
+    });
+
+    var link = svg.append("g")
+        .style("stroke", "#aaa")
+        .selectAll("line")
+        .data(graph.links)
+        .enter().append("line");
+
+    var node = svg.append("g")
+        .attr("class", "nodes")
+        .selectAll("circle")
+        .data(graph.nodes)
+        .enter().append("circle")
+        .attr("r", 2)
+        .call(d3.drag()
+            .on("start", dragstarted)
+            .on("drag", dragged)
+            .on("end", dragended));
+
+    var label = svg.append("g")
+        .attr("class", "labels")
+        .selectAll("text")
+        .data(graph.nodes)
+        .enter().append("text")
+        .attr("class", "label")
+        .text(function(d) { return d.id; });
+
+    simulation
+        .nodes(graph.nodes)
+        .on("tick", ticked);
+
+    simulation.force("link")
+        .links(graph.links);
+
+    function ticked() {
+        link
+            .attr("x1", function(d) { return d.source.x; })
+            .attr("y1", function(d) { return d.source.y; })
+            .attr("x2", function(d) { return d.target.x; })
+            .attr("y2", function(d) { return d.target.y; });
+
+        node
+            .attr("r", 40)
+            .style("fill", "#efefef")
+            .style("stroke", "#424242")
+            .style("stroke-width", "1px")
+            .attr("cx", function (d) { return d.x+35; })
+            .attr("cy", function(d) { return d.y-3; });
+
+        label
+            .attr("x", function(d) { return d.x; })
+            .attr("y", function (d) { return d.y; })
+            .style("font-size", "13px").style("fill", "#333");
+    }
+}
+
+function dragstarted(d) {
+    if (!d3.event.active) simulation.alphaTarget(0.3).restart()
+    d.fx = d.x
+    d.fy = d.y
+//  simulation.fix(d);
+}
+
+function dragged(d) {
+    d.fx = d3.event.x
+    d.fy = d3.event.y
+//  simulation.fix(d, d3.event.x, d3.event.y);
+}
+
+function dragended(d) {
+    d.fx = d3.event.x
+    d.fy = d3.event.y
+    if (!d3.event.active) simulation.alphaTarget(0);
+    //simulation.unfix(d);
+}
+run(graph)
